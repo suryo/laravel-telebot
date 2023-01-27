95d943760fbf344cadb93b69cec00cffef84177c
suryo <suryoatm@gmail.com>
2023-01-27 16:09:57 +0700

    modified webhook

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -8,26 +8,27 @@ use Telegram\Bot\Api;
 class TelegramBotController extends Controller
 {
     
-    public function __construct()
-    {
-        $this->telegram = new API(env('TELEGRAM_BOT_TOKEN'));
-    }
+    // public function __construct()
+    // {
+    //     $this->telegram = new API(env('TELEGRAM_BOT_TOKEN'));
+    // }
 
-    public function sendMessages($id)
-    {
-        return  $this->telegram->sendMessage([
-            'chat_id' => $id,
-            'text' => 'Hello...Iam SuryoTeleBot'
-        ]);
-    }
+    // public function sendMessages($id)
+    // {
+    //     return  $this->telegram->sendMessage([
+    //         'chat_id' => $id,
+    //         'text' => 'Hello...Iam SuryoTeleBot'
+    //     ]);
+    // }
 
-    public function messages()
-    {
-        return  $this->telegram->getUpdates();
-    }
+    // public function messages()
+    // {
+    //     return  $this->telegram->getUpdates();
+    // }
 
     public function telegramWebhook()
     {
+        $this->telegram = new API(env('TELEGRAM_BOT_TOKEN'));
         $updates = $this->telegram->getWebhookUpdates();
 
         if (isset($updates['message'])) {
